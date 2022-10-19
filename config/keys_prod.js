0fe677907eccb4ba1a8490c7e57d39aed92ae038
Balram Yadav <76912100+BalramYadav00@users.noreply.github.com>
2022-10-19 20:06:30 +0530

    Update keys_prod.js

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI:"mongodb+srv://<username>:LdrUJOoOMqu1vPlh@cluster0-azhuz.mongodb.net/admin?retryWrites=true&w=majority",
+  mongoURI:"mongodb+srv://Jhonny:LdrUJOoOMqu1vPlh@cluster0-azhuz.mongodb.net/admin?retryWrites=true&w=majority",
   secretOrKey: "Bearer",
 };
